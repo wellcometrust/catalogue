b53e586c7b2bf3730edc173c2d1a3c6aa88dc4c4
Alex Chan <a.chan@wellcome.org>
2020-11-24 09:22:05 +0000

    Add a new parameter "isOnlyMets"

--- mets_adapter/mets_adapter/src/main/scala/uk/ac/wellcome/mets_adapter/models/MetsLocation.scala
+++ mets_adapter/mets_adapter/src/main/scala/uk/ac/wellcome/mets_adapter/models/MetsLocation.scala
@@ -12,7 +12,15 @@ case class MetsLocation(
   version: Int,
   file: String,
   createdDate: Instant,
-  manifestations: List[String] = Nil
+  manifestations: List[String] = Nil,
+  // If Digital Production want to "delete" a bag, they send a new version
+  // through Goobi that doesn't have any files.
+  //
+  // The bag will contain a METS file, and nothing else.  e.g. b1665836x v3
+  //
+  // This flag records whether a bag only contains the METS file, which is
+  // a clue to us that we should ignore it.
+  isOnlyMets: Boolean = false
 ) {
 
   private def s3Prefix = S3ObjectLocationPrefix(bucket, keyPrefix = path)
