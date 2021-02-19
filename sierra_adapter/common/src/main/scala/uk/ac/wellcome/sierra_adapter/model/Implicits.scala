e20d9e2373981634159b27cb575b75650782bcc1
Alex Chan <a.chan@wellcome.org>
2021-02-19 08:58:16 +0000

    Move the DynamoDB implicits to a shared package

--- sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/model/Implicits.scala
+++ sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/model/Implicits.scala
@@ -2,9 +2,24 @@ package uk.ac.wellcome.sierra_adapter.model
 
 import io.circe.generic.extras.semiauto._
 import io.circe._
+import org.scanamo.DynamoFormat
 import uk.ac.wellcome.json.JsonUtil._
 
 object Implicits {
+  implicit val formatBibNumber: DynamoFormat[SierraBibNumber] =
+    DynamoFormat
+      .coercedXmap[SierraBibNumber, String, IllegalArgumentException](
+        SierraBibNumber,
+        _.withoutCheckDigit
+      )
+
+  implicit val formatItemNumber: DynamoFormat[SierraItemNumber] =
+    DynamoFormat
+      .coercedXmap[SierraItemNumber, String, IllegalArgumentException](
+        SierraItemNumber,
+        _.withoutCheckDigit
+      )
+
   // Because the [[SierraTransformable.itemRecords]] field is keyed by
   // [[SierraItemNumber]] in our case class, but JSON only supports string
   // keys, we need to turn the ID into a string when storing as JSON.
